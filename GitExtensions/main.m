56677a5cdfaa42c4e54facff3dd00a0b86d39ca7
Cristian Kocza <cristian.kocza@sandibel-4.local>
2012-04-06 16:36:08 +0300

    Initial Commit

---
+++ GitExtensions/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  GitExtensions
+//
+//  Created by Cristian Kocza on 06.04.2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
